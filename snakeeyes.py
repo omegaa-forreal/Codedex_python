6b9c121b38d5322f312d3080eeb024d46e972573
omegaa <omegax3d.yt@gmail.com>
2025-08-12 16:39:54 +0200

    Create snakeeyes.py

---
+++ snakeeyes.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+import random
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+
+total = die1 + die2 
+
+while total!=2:
+  print("Nope")
+  die1 = random.randint(1, 6)
+  die2 = random.randint(1, 6)
+  total=die1+die2
+  if total == 2:
+    print('Snake eyes')
